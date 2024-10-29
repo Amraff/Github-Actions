5b8b5261bfbc9a7fab17a316a64df7994a5f3ea0
Raphew <raphewkarim@gmail.com>
2024-10-29 02:07:06 +0000

    update ascii workflow file

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+# /bin/bash
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON......RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt 
 ls -ltra
-pwd
